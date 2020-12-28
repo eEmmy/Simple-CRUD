6ab6871f89ec0d3c77468d96f9aac66b467f621d
eEmmy <aou-emmy@outlook.com>
2020-12-28 07:47:08 -0300

    Final update to 2.0.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '2.0.0',
+    'version' => '2.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'eemmy/simple-crud',
+  ),
+  'versions' => 
+  array (
+    'eemmy/simple-crud' => 
+    array (
+      'pretty_version' => '2.0.0',
+      'version' => '2.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
